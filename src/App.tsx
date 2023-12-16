ba4fa6edb3108a082678693b6b39d8fb94452bbb
snktuary <153958772+snktuary@users.noreply.github.com>
2023-12-16 02:15:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
       </header>
     </div>
   )
