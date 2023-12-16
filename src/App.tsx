0b6540c4fe999ef06479afd45c882fda6c039dfb
snktuary <153958772+snktuary@users.noreply.github.com>
2023-12-16 02:23:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Prioritize your well-being and grow through meaningful activities</p>
       </header>
     </div>
   )
